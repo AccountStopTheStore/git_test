c6bc921108296b1d60beef3de3515806abe25431
Whale2200d <skatewang99@gmail.com>
2023-10-30 10:37:41 +0900

    Feat : index.js 파일 추가

---
+++ index.js
@@ -0,0 +1,21 @@
+/** 얕은 복사 */
+const info = { name: "inseop", age: 30, job: "sw engineer" };
+const info2 = info;
+info2.name = "gyeongrak";
+
+console.log(info);
+console.log(info2);
+console.log(info === info2); // true
+
+/** 깊은 복사 */
+// const info = {
+//   name: "inseop",
+//   age: 30,
+//   job: "sw engineer",
+//   address: { city: "seoul", post: 123 },
+// };
+// const info2 = { ...info };
+// info2.address.city = "busan";
+
+// console.log(info);
+// console.log(info2);
